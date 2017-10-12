bafa8f5d63719f9a962b7a931392eff304b78fd4
Leonardo Alves <leunardosevla@gmail.com>
2017-10-12 18:19:26 -0300

    feat: added keywords variation

--- spec/keywordsSpec.js
+++ spec/keywordsSpec.js
@@ -33,4 +33,40 @@ describe("When text contains a keyword", () => {
         expect(translate('respeita')).toBe("rx o pai '-'");
     })
 
+    it("should translate 'que' to 'eoq", () => {        
+        expect(translate('que')).toBe("eoq");
+    })
+
+    it("should translate 'porra' to 'hiiieiieihhieihieieieeieejehiehehieeheie", () => {        
+        expect(translate('porra')).toBe("hiiieiieihhieihieieieeieejehiehehieeheie");
+    })
+
+    it("should translate 'sim' to 'ss fon trab", () => {        
+        expect(translate('sim')).toBe("ss fon trab");
+    })  
+    
+    it("should translate 'ss' to 'ss fon trab", () => {        
+        expect(translate('ss')).toBe("ss fon trab");
+    }) 
+
+    it("should translate 'não' to 'nn todeboa", () => {        
+        expect(translate('não')).toBe("nn todeboa");
+    })
+
+    it("should translate 'nao' to 'nn todeboa", () => {        
+        expect(translate('nao')).toBe("nn todeboa");
+    })
+
+    it("should translate 'nn' to 'nn todeboa", () => {        
+        expect(translate('nn')).toBe("nn todeboa");
+    })
+
+    it("should translate 'chupa e engole' to 'chupingole", () => {        
+        expect(translate('chupa e engole')).toBe("chupingole");
+    })
+
+    it("should translate 'cu' to 'cudegualipito", () => {        
+        expect(translate('cu')).toBe("cudegualipito");
+    })
+
 });
