3130341df0387a1b40b25fb1a800ce6b7cacd225
Leonardo Alves <leunardosevla@gmail.com>
2017-10-22 15:14:39 -0200

    add cors allow methods

--- server/main.js
+++ server/main.js
@@ -7,8 +7,10 @@ const app = express();
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'POST, OPTIONS');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+
+    if(req.method === 'OPTIONS') res.send(200); else next();
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
