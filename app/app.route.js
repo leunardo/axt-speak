ac4fca56f5ba660ea0c7d80b7aca0ca4ad790a5e
Leonardo Alves <leunardosevla@gmail.com>
2017-10-22 13:10:07 -0200

    change route

--- app/app.route.js
+++ app/app.route.js
@@ -1,7 +1,7 @@
 angular.module('app').config(function ($routeProvider){
     
     $routeProvider.when('/xisquedele', {
-        templateUrl: './axt-speak/app/xisquedele/index.html',
+        templateUrl: './app/xisquedele/index.html',
         controller: 'xisquedeleController'
     })
 
